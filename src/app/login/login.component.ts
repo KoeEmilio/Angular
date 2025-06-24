5320061e3f1a907d1f0ee4fb98bb99605116ef55
AxelYYa <axel.y.ya.0@gmail.com>
2025-06-24 13:07:49 -0600

    v1.1 - Cambios de usos Inject

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -11,7 +11,8 @@ import { AuthService } from '../services/auth.service';
   `
 })
 export class LoginComponent {
-  constructor(private auth: AuthService, private router: Router) {}
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
   login() {
     this.auth.login();
